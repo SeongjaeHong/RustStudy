a2c4c59ed68398600777291d2f8813e15e381568
Seongjae Hong <sjhong310@gmail.com>
2024-04-29 00:55:02 +0900

    Impl

--- basic_concepts/src/main.rs
+++ basic_concepts/src/main.rs
@@ -1,12 +1,22 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let mut s = String::from("hello");
-
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{} and {}", r1, r2);
-    
-
-    let r3 = &mut s; // Can't refer to mutable at the same time with a refence to unmutable
-    println!("changed s : {r3}");
-    println!("x is {r1}");
+    let rect1 = Rectangle::square(5);
+    println!("area: {}", rect1.area());
 }
