8f226b18631208610443f54831acbc8ff60e9cfd
Seongjae Hong <sjhong310@gmail.com>
2024-04-28 16:15:33 +0900

    Remote target

--- hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d
+++
@@ -1,5 +0,0 @@
-/Users/sjhong/Documents/Rust/study/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03: src/main.rs
-
-/Users/sjhong/Documents/Rust/study/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/release/deps/hello_cargo-0776c85fc5514e48.d
+++
@@ -1,5 +0,0 @@
-/Users/sjhong/Documents/Rust/study/hello_cargo/target/release/deps/hello_cargo-0776c85fc5514e48: src/main.rs
-
-/Users/sjhong/Documents/Rust/study/hello_cargo/target/release/deps/hello_cargo-0776c85fc5514e48.d: src/main.rs
-
-src/main.rs:
