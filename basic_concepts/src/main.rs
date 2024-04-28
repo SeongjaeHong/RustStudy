523bad1cce97551df96bca716c1b417783f97567
Seongjae Hong <sjhong310@gmail.com>
2024-04-28 23:33:57 +0900

    Reference

--- basic_concepts/src/main.rs
+++ basic_concepts/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    
+
+    let r3 = &mut s; // Can't refer to mutable at the same time with a refence to unmutable
+    println!("changed s : {r3}");
+    println!("x is {r1}");
 }
