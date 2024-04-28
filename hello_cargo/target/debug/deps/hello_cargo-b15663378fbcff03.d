5166f15b6b86e315e9ec4ce9358524323b51b209
Seongjae Hong <sjhong310@gmail.com>
2024-04-28 15:49:09 +0900

    Init

---
+++ hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d
@@ -0,0 +1,5 @@
+/Users/sjhong/Documents/Rust/study/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03: src/main.rs
+
+/Users/sjhong/Documents/Rust/study/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/sjhong/Documents/Rust/study/hello_cargo/target/release/hello_cargo: /Users/sjhong/Documents/Rust/study/hello_cargo/src/main.rs
