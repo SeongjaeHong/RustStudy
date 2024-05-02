915e67f6d7d667aa0a5dc7a9d8fadcf7b03e46ee
Seongjae Hong <sjhong310@gmail.com>
2024-05-03 00:58:39 +0900

    generic type

--- basic_concepts/src/main.rs
+++ basic_concepts/src/main.rs
@@ -25,11 +25,29 @@ impl Rectangle {
     }
 }
 
+use std::cmp::PartialOrd;
+
+struct Point<X1, Y1> {
+    x1: X1,
+    y1: Y1
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, a>(self, other: Point<X2, a>) -> Point<X1, a> {
+        Point {
+            x1: self.x1,
+            y1: other.y1
+        }
+    }
+}
+
 fn main() {
-    
-    println!("Lets' go\n");
-    let x = err_func();
-    println!("{:?}", x);
+    // let x = err_func();
+    // println!("{:?}", x);
+    let p1: Point<i32, f64> = Point {x1:3,y1:5.2};
+    let p2 = Point {x1: 'a', y1: "hello"};
+    let p3 = p1.mixup(p2);
+    println!("p3.x: {}, p3.y: {}", p3.x1, p3.y1);
 }
 
 fn err_func() -> Result<String, io::Error> {
