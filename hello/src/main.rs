3fdf042ebeeb3b994133da9dee0482dc38bec28e
Seongjae Hong <sjhong310@gmail.com>
2024-05-12 00:36:15 +0900

    Shorten code

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,7 +1,9 @@
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration
 };
 
 fn main() {
@@ -16,19 +18,18 @@ fn main() {
 fn handle_conntection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, file_path) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "./pages/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "./pages/404.html")
+
+    let (status_line, file_path) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./pages/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "./pages/hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "./pages/404.html")
     };
+
     let contents = fs::read_to_string(file_path).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
-    
-    // let http_request: Vec<_> = buf_reader
-    // .lines()
-    // .map(|line| line.unwrap())
-    // .take_while(|line| !line.is_empty())
-    // .collect();
 }
